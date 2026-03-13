d76b6b81e5e98618d3c82214fdf5537593b336c3
kauan de jesus flöter <kauanflo@hotmail.com>
2026-03-13 04:27:20 -0300

    feat: Faz mapeamento de 1d para 2d da variavel data

--- main_codes/main.c
+++ main_codes/main.c
@@ -13,18 +13,18 @@ int main(){
     // Printa os valores que acl função stbi_load() econtrou acima
     printf("%d %d %d \n",pLargura, pAltura, pQuant_canais);
 
-    int valores[pAltura][pLargura];
-    valores[0][0] = pAltura, pLargura;
-
-    /*for (int *v; v == valores[pAltura]; v = valores[pAltura] + 1){
-        for ( int *h; h == valores[pLargura]; h = valores[pLargura] + 1){
-            printf("%d", valores[0][0]);
+    for (int i = 0; i < pAltura; i++){
+        for ( int j = 0; j < pLargura; j++){
+            
+            int index= (i * pLargura + j) * 3;
+            printf("%d %d %d \n", data[index], data[index + 1], data[index + 2 ]);
         }
-        printf("%d", valores[0][0]);
+        //int indexAltura = (j *pAltura + i) * 3;
+        // printf("%d", pAltura);
     }
-    */
+    
 
-    printf("%d %d\n", valores[0][0]);
-    extern void stbi_image_free(void *retval_from_stbi_load); // limpa a imagem pra evitar memory leak
+
+    stbi_image_free(data); // limpa a imagem pra evitar memory leak
     return 0;
 }
