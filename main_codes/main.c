9235d61985fe7ecdd0ad8e6400394ff2c3819498
kauan de jesus flöter <kauanflo@hotmail.com>
2026-03-13 05:31:17 -0300

    fix: Ajusta a Fórmula na fómula de luminosidade e corrige a divisão decimal

--- main_codes/main.c
+++ main_codes/main.c
@@ -16,14 +16,26 @@ int main(){
     for (int i = 0; i < pAltura; i++){
         for ( int j = 0; j < pLargura; j++){
             
+            // logica de mapeamento de array 1d (data) para array 2d (index)
             int index= (i * pLargura + j) * 3;
-            printf("%d %d %d \n", data[index], data[index + 1], data[index + 2 ]);
-        }
-        //int indexAltura = (j *pAltura + i) * 3;
-        // printf("%d", pAltura);
-    }
-    
+            
+            // trasnforma os valores rgb anteriores em um unico valor de luminosidade
+            float luminosidade = ((0.02126 * data[index]) + (0.7152 * data[index + 1]) + (0.0722 * data[index + 2]));
+            
+            char *caracteres = "@%#*+=-:. ";
 
+            // Coloca o "nivel" de luminosidade entre 0.00 e 9.00 (para os 9 caracteres que iremos usar)
+            float indiceAscii = ((luminosidade / 255.0 ) * 9);
+            
+            // Transforma o indiceAscii de float para int (pra poder ser um index dos caracteres)
+            int castedIndiceAscii = indiceAscii;
+
+            printf ("%c", caracteres[castedIndiceAscii]);
+
+            // printf("%c", caracteres[indiceAscii]);
+        }
+        printf("%\n");
+    }
 
     stbi_image_free(data); // limpa a imagem pra evitar memory leak
     return 0;
