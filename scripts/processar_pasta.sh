a8323d17e0493522d1bd428fb72d9b5067d27998
kauan de jesus flöter <kauanflo@hotmail.com>
2026-03-14 16:00:06 -0300

    feat: Adiciona script de processamento batch para multiplas imagens

---
+++ scripts/processar_pasta.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p resultados
+mkdir -p bin
+
+gcc src/main.c -lm -o bin/IMG_to_ASCII.exe
+
+for imagem in imagem_entrada/*.png; do\
+    nome_da_imagem=$(basename "$imagem" .png)
+    echo "🔁 Convertendo $nome_da_imagem..."
+
+    .bin/IMG_to_ASCII.exe "$imagem" > "resultados/${$nome_da_imagem}.txt"
+done
+
+echo "✅ Tudo pronto ! Verifique a pasta de 'resultados' para encontrar a sua arte ASCII !."
