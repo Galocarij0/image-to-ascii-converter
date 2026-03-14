a465b406f95c02525c176e8ceb8a7eabe0c3d206
kauan de jesus flöter <kauanflo@hotmail.com>
2026-03-14 16:42:20 -0300

    feat: Adiciona suporte para multiplos formatos de imagem no processamento batch

--- scripts/processar_pasta.sh
+++ scripts/processar_pasta.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
+shopt -s extglob 
+
+VERDE='\033[0;32m'
+AZUL='\033[0;34m'
+NOCOLOR='\033[0;0m'
+
+clear
+echo -e "${AZUL}=====================================================${NOCOLOR}"
+echo -e "${AZUL}   🚀   TRANSFORMANDO IMAGEM EM ARTE ASCII    🚀    ${NOCOLOR}"
+echo -e "${AZUL}=====================================================${NOCOLOR}"
 
 mkdir -p resultados
 mkdir -p bin
 
-gcc src/main.c -lm -o bin/IMG_to_ASCII.exe
+gcc src/main.c -lm -o bin/IMG_to_ASCII.exe || { echo "❌ Erro Fatal: Houve uma falha na compilação :("; exit 1;}
 
-for imagem in imagens_entrada/*.png; do
-    nome_da_imagem=$(basename "$imagem" .png)
-    echo "🔁 Convertendo $nome_da_imagem..."
+for imagem in imagens_entrada/*.@(png|jpg|jpeg); do
+    [ -e "$imagem" ] || continue
+    if [ -f "$imagem" ]; then
+        nome_da_imagem=$(basename "$imagem" .${imagem##*.})
+        nome_da_imagem="${nome_da_imagem%.*}"
+        echo -e "🔁 Convertendo ${VERDE}$nome_da_imagem${NOCOLOR}..."
 
-    ./bin/IMG_to_ASCII.exe "$imagem" > "resultados/${nome_da_imagem}.txt"
+        ./bin/IMG_to_ASCII.exe "$imagem" > "resultados/${nome_da_imagem}.txt"
+    fi
 done
 
-echo "✅ Tudo pronto ! Verifique a pasta de 'resultados' para encontrar a sua arte ASCII !."
+echo -e "\n${VERDE}✅ Tudo pronto ! Verifique a pasta de 'resultados' para encontrar a sua arte ASCII !${NOCOLOR}."
